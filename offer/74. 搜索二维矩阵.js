aa9e1dbe60ae9d8cff6ed18391044db9030aa5b0
Felix.Price <zhupengfei1995@gmail.com>
2021-08-10 22:47:54 +0800

    feat: add 74

---
+++ offer/74. 搜索二维矩阵.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    if (!matrix.length) return false;
+    let row = matrix.length - 1,
+        col = 0;
+
+    // Note: 因为当前这个数字大于它上面的数，小于它右边的数，从左下角开始查找
+    while (row >= 0 && col < matrix[0].length) {
+        if (matrix[row][col] > target) {
+            row--;
+        } else if (matrix[row][col] < target) {
+            col++;
+        } else return true;
+    }
+    return false;
+};
+
+console.log(
+    searchMatrix(
+        [
+            [1, 3, 5, 7],
+            [10, 11, 16, 20],
+            [23, 30, 34, 60],
+        ],
+        3
+    )
+);
+console.log(
+    searchMatrix(
+        [
+            [1, 3, 5, 7],
+            [10, 11, 16, 20],
+            [23, 30, 34, 60],
+        ],
+        13
+    )
+);
