cf69e729ce136ca9559b5a5f51dbdd5c40994e99
Felix.Price <zhupengfei1995@gmail.com>
2021-12-06 22:43:29 +0800

    feat: day12 杨辉三角II

---
+++ dp-plan/day12/119. 杨辉三角 II.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+    const numRows = rowIndex + 1;
+    let dp = [];
+    for (let i = 0; i < numRows; i++) {
+        dp[i] = new Array(i + 1).fill(0);
+        dp[i][0] = 1;
+    }
+
+    for (let i = 1; i < numRows; i++) {
+        for (let j = 1; j <= i; j++) {
+            if (i === j || j === 0) {
+                dp[i][j] = 1;
+            } else {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+    }
+    console.log(dp);
+    return dp[rowIndex];
+};
+
+console.log(getRow(3));
