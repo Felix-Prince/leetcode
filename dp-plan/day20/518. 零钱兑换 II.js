20c555f1eeeed12d963bd997996324f95bebf843
Felix.Price <zhupengfei1995@gmail.com>
2021-12-20 22:48:08 +0800

    feat: day20

---
+++ dp-plan/day20/518. 零钱兑换 II.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+    const dp = new Array(amount + 1).fill(0);
+    dp[0] = 1;
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+    return dp[amount];
+};
+
+console.log(change(5, [1, 2, 5]));
+console.log(change(3, [2]));
+console.log(change(10, [10]));
