d991d3fcb11b3fba25849c3b50588dc469392f6b
Felix.Price <zhupengfei1995@gmail.com>
2021-12-23 22:25:57 +0800

    fix: algorithms day01

---
+++ algorithms/day01/704. 二分查找.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0,
+        right = nums.length;
+
+    while (left < right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return -1;
+};
+
+// console.log(search([-1, 0, 3, 5, 9, 12], 9));
+// console.log(search([-1, 0, 3, 5, 9, 12], 2));
+console.log(search([-1, 0, 5], 5));
